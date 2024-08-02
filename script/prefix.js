7bb7e8d1c85e17103b99fb94477901d4ff9dd0ad
Zeus <160679258+Ulric-aty@users.noreply.github.com>
2024-08-02 23:29:23 +0100

    Add files via upload

---
+++ script/prefix.js
@@ -0,0 +1,27 @@
+
+const fs = require("fs");
+module.exports.config = {
+name: "prefix",
+    version: "1.0.1",
+hasPermssion: 0,
+credits: "Ulric dev", 
+description: "no prefix",
+commandCategory: "No command marks needed",
+usages: "...",
+    cooldowns: 1, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+var { threadID, messageID } = event;
+if (event.body.indexOf("prefix")==0 || (event.body.indexOf("Prefix")==0 || (event.body.indexOf("Ano prefix")==0 || (event.body.indexOf("ano prefix")==0)))) {
+    const moment = require("moment-timezone");
+    var gio = moment.tz("Africa/Porto-Novo").format("HH:mm:ss || D/MM/YYYY");
+var msg = {
+body: `⚙️My prefix is: 【 ${global.config.PREFIX} 】`
+}
+api.sendMessage(msg, threadID, messageID);
+}
+}
+module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+}
