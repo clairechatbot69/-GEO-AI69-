459ba60546776c278c931c8a0747163451671974
Zeus <160679258+Ulric-aty@users.noreply.github.com>
2024-08-07 01:28:08 +0100

    Add files via upload

--- script/ai2.js
+++ script/ai2.js
@@ -1,34 +1,32 @@
 const axios = require('axios');
-
 module.exports.config = {
-  name: 'ai2',
+  name: 'ai',
   version: '1.0.0',
   role: 0,
   hasPrefix: false,
   aliases: ['gpt', 'openai'],
   description: "An AI command powered by GPT-4",
-  usage: "Ai [question]",
-  credits: 'joshua Apostol',
+  usage: "Ai [promot]",
+  credits: 'Men',
   cooldown: 3,
 };
-
-module.exports.run = async function({ api, event, args }) {
+module.exports.run = async function({
+  api,
+  event,
+  args
+}) {
   const input = args.join(' ');
-
   if (!input) {
-    api.sendMessage(`Please provide a question or statement after 'gpt4o'. For example: 'gp4o What is the capital of France?'`, event.threadID, event.messageID);
+    api.sendMessage(`Hello how I can help you today?`, event.threadID, event.messageID);
     return;
   }
-
-  api.sendMessage('Please wait...', event.threadID, event.messageID);
-
+  api.sendMessage(``, event.threadID, event.messageID);
   try {
-    const { data } = await axios.get(`https://nash-api-end.onrender.com/freegpt4o8k?question=${encodeURIComponent(input)}`);
-    let response = JSON.parse(data.answer).response;
-    
-    response = response.replace(/\n\nIs this answer helpful to you\?/, '');
-
-    api.sendMessage(response, event.threadID, event.messageID);
+    const {
+      data
+    } = await axios.get(`https://jonellccprojectapis10.adaptable.app/api/ai?query=${encodeURIComponent(input)}`);
+    const response = data.response;
+    api.sendMessage( response + , event.threadID, event.messageID);
   } catch (error) {
     api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
   }
